227c83e57f89ad641dd450cfd143cb1c595c48c9
adkaizer7 <adkaizer7@gmail.com>
2014-12-03 15:44:54 -0800

    Cleaning up

--- src/edu/berkeley/monitoring/util/bluetooth/BTDeviceHandlerInterface.java
+++
@@ -1,7 +0,0 @@
-package edu.berkeley.monitoring.util.bluetooth;
-
-public interface BTDeviceHandlerInterface {
-	void onReceive(BTSendableInterface<?> o);
-	void onFailure(Exception e);
-	void onConnect();
-}
--- src/edu/berkeley/monitoring/util/bluetooth/BTSendableInterface.java
+++
@@ -1,7 +0,0 @@
-package edu.berkeley.monitoring.util.bluetooth;
-
-import java.io.Serializable;
-
-public interface BTSendableInterface<T> extends Serializable {
-
-}
